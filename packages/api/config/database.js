2b279a2c30a5130c0236f6c6f761fe28cc9ac6a5
Thiago Mendonca <trystan2k@gmail.com>
2020-10-25 09:47:32 +0100

    chore: update database config

--- packages/api/config/database.js
+++ packages/api/config/database.js
@@ -4,12 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        port: env('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: { rejectUnauthorized: false }
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
+      options: {}
+    }
+  }
 });
